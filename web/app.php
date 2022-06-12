741d187e2048f78ba34b990f1652a728aea3c381
rochmadnf <rochmadnf@gmail.com>
2022-06-12 14:18:49 +0800

    get data from antares srver

--- web/app.php
+++ web/app.php
@@ -16,3 +16,14 @@ $headers = [
 $client = new Client();
 
 // methods
+
+// get data terakhir yang masuk ke server antares
+function getData()
+{
+  header("Content-Type: application/json");
+  global $client, $headers;
+  $response = $client->get(CONFIG_ANTARES_URL . '/la', ['headers' => $headers])->getBody()->getContents();
+  echo $response;
+}
+
+getData();
