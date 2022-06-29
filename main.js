2acaf86cea50d3b10132c0f19def78e9ab7b1ed0
FathurAlamsyah <andyfathur121@gmail.com>
2022-06-29 19:23:42 +0800

    tes ubah

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 const url = `${location.protocol}//${location.host}`;
-const checkButton = document.getElementById('btn-check');
+const checkButton = document.getElementById("btn-check");
 
 async function loadData() {
-    const request = await fetch(`${url}/app.php`).then(res => res.json());
-    const data = JSON.parse(request["m2m:cin"].con);
-    const display = document.getElementById('data-display');
+  const request = await fetch(`${url}/app.php`).then((res) => res.json());
+  const data = JSON.parse(request["m2m:cin"].con);
+  const display = document.getElementById("data-display");
 
-    // untuk display data ke halaman website
-    display.childNodes[1].textContent = `pH : ${data.ph}`;
-    display.childNodes[3].textContent = `${data.turbidity} NTU`;
-    display.childNodes[5].textContent = `${data.temperature} ℃`;
+  // untuk display data ke halaman website
+  display.childNodes[1].textContent = `${data.ph}`;
+  display.childNodes[3].textContent = `${data.turbidity} NTU`;
+  display.childNodes[5].textContent = `${data.temperature} ℃`;
 
-    // display waktu
-    document.getElementById('date-display').textContent = moment.unix(data.current_time).format('DD/MM/YYYY');
-    document.getElementById('time-display').textContent = moment.unix(data.current_time).format('HH:mm:ss');
+  // display waktu
+  document.getElementById("date-display").textContent = moment.unix(data.current_time).format("DD/MM/YYYY");
+  document.getElementById("time-display").textContent = moment.unix(data.current_time).format("HH:mm:ss");
 }
-checkButton.addEventListener('click', loadData);
+checkButton.addEventListener("click", loadData);
